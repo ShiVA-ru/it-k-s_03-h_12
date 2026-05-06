0c87a31ba91664cb2d798e99a274c238399da9e5
Shinyaev Vladimir <secman@bk.ru>
2026-05-06 21:29:44 +0500

    add types for likes in posts entitiy

--- src/features/blogs/domain/blog.entity.ts
+++ src/features/blogs/domain/blog.entity.ts
@@ -2,12 +2,25 @@ import type { HydratedDocument, Model } from "mongoose";
 import mongoose, { model } from "mongoose";
 import type { CreateBlogDto, UpdateBlogDto } from "../types/blog.dto.js";
 
+export type NewestLike = {
+	userId: string;
+	login: string;
+	createdAt: string; //in ViewModel - addedAt
+}
+
+export type ExtendedLikes = {
+	likesCount: number;
+	dislikesCount: number;
+	newestLikes: NewestLike[]
+}
+
 export type Blog = {
 	name: string;
 	description: string;
 	websiteUrl: string;
 	createdAt: Date;
 	isMembership: boolean;
+	extendedLikesInfo: ExtendedLikes;
 };
 
 interface BlogMethods {
@@ -19,6 +32,17 @@ type BlogStatic = typeof BlogEntity;
 type BlogModel = Model<Blog, {}, BlogMethods> & BlogStatic;
 
 export type BlogDocument = HydratedDocument<Blog, BlogMethods>;
+
+const newestLikesShema = new mongoose.Schema<NewestLike>({
+	userId: String,
+	login: String,
+}, { timestamps: true });
+
+const ExtendedLikesSchema = new mongoose.Schema<ExtendedLikes>({
+	likesCount: { type: Number, default: 0 },
+	dislikesCount: { type: Number, default: 0 },
+	newestLikes: { type: [newestLikesShema], default: [] }
+})
 
 const BlogSchema = new mongoose.Schema<Blog, BlogModel, BlogMethods>({
 	name: { type: String, trim: true, required: true },
